3bcb716d1077484da029b1a37e66f0fb1f823238
Javier Santana <javier@javier.nyc>
2021-05-14 16:04:08 -0400

    addition

---
+++ Sources/UhSpotNavigation/UhSpotNavigation.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapboxNavigation.
+FOUNDATION_EXPORT double UhSpotNavigationVersionNumber;
+
+//! Project version string for MapboxNavigation.
+FOUNDATION_EXPORT const unsigned char UhSpotNavigationVersionString[];
+
+#import "CLLocationManager+MGLNavigationAdditions.h"
+#import "MGLMapView+MGLNavigationAdditions.h"
