3bc6dd815c943c5b25088dfba5664acb9be98cda
JavierSantanaNYC <javier@javier.nyc>
2021-06-17 00:00:46 -0400

    constraints

--- Sources/UhSpotNavigation/NavigationViewLayout.swift
+++ Sources/UhSpotNavigation/NavigationViewLayout.swift
@@ -2,7 +2,9 @@ import UIKit
 
 extension NavigationView {
     func setupConstraints() {
+        mapView.topAnchor.constraint(equalTo: topAnchor).isActive = true
         mapView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         mapView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
   
      
